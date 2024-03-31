b11af4e914283450e9741d0dea93867081fc8e62
Omkar0109 <sobp9999@gmail.com>
2024-04-01 01:20:15 +0530

    phase2

--- tictactoeapp/src/Square/Square.jsx
+++ tictactoeapp/src/Square/Square.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import "./Square.css";
 
 
-
-
 const Square = (setGameState, id) => {
   return <div className="square"></div>;
 };
+// Adittya 
 
 export default Square;
